a3e45280cea8c0f43f7a4a5a15c1fbf41a1a1154
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2026-06-12 10:50:28 +0900

    fix(api): export context from HawtioPage UI component

--- packages/hawtio/src/ui/index.ts
+++ packages/hawtio/src/ui/index.ts
@@ -1,6 +1,6 @@
-export * from './hooks'
-export { HawtioLoadingPage } from './page'
-export { FilteredTable, ExpandableText } from './util'
 export { Hawtio } from '@hawtiosrc/Hawtio'
+export * from './hooks'
+export { HawtioLoadingPage, PageContext } from './page'
+export { ExpandableText, FilteredTable } from './util'
 // Hawtio React component
 export * from './../Hawtio'
